9ed7005b9f1972136be97f3bf2d1d134730d6b5c
difoxy2 <bigdk0900@gmail.com>
2025-01-01 11:12:59 +0800

    Plot fake data dinanmically

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
 st.title('Weather Forecast for the Next Days')
 
@@ -9,3 +11,21 @@ days=st.slider('Forecast Days',1,5, help='Slide to see data for more dates')
 option=st.selectbox('Select data to view',('Tempeture','Sky'))
 
 st.subheader(f'{option} for the next {days} days in {place}')
+
+
+#plotdata = {
+#        "date":['2025-1-2','2025-1-3','2025-1-4'],
+#        "temp":['30','32','29'],
+#    }
+#plotdata['temp'] = [days*int(i) for i in plotdata['temp']]
+
+#st.line_chart(pd.DataFrame(plotdata),x='date',y='temp')
+
+
+dates=['2025-1-2','2025-1-3','2025-1-4']
+temps=[30,32,29]
+temps=[days*int(i) for i in temps]
+
+plot=px.line(x=dates,y=temps,labels={'x':'Date','y':'Tempeture (C)'})
+st.plotly_chart(plot)
+
--- readne.md
+++ readne.md
@@ -1,3 +1,12 @@
 # Weather Forcast Dashboard
 
 ## What is this app?
+
+## Library used in this app:
+
+* Python
+* User interface: Stream lit
+* Plot Graph: Ploty
+* Data source:
+
+### This app is homework App 7 - Build a Weather Forecast Dashboard from Udemy course [Python Mega Course: Learn Python in 60 Days, Build 20 Apps](https://www.udemy.com/course/the-python-mega-course/learn/lecture/34604706#overview)
